1d265a700624d47aec6fdc0f0b7a47bd0d34669c
Arthur Evans <arthure@google.com>
2020-09-23 14:59:03 -0700

    Update sample metadata for code-sample-editor.

--- packages/lit-dev-content/.eleventy.js
+++ packages/lit-dev-content/.eleventy.js
@@ -25,6 +25,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('site/css');
   eleventyConfig.addPassthroughCopy('site/images/**/*');
   eleventyConfig.addPassthroughCopy('api/**/*');
+  eleventyConfig.addPassthroughCopy({'site/_includes/projects': 'samples'});
   eleventyConfig.addPassthroughCopy({'node_modules/code-sample-editor/typescript-worker.js': './typescript-worker.js'});
   eleventyConfig.addPassthroughCopy({'node_modules/code-sample-editor/service-worker.js': './service-worker.js'});
 
