29a8af9bdf12c77c471689890025b97bcc26f37e
Alexander Marks <aomarks@google.com>
2020-12-18 10:44:58 -0800

    Remove unused side-nav element

--- packages/lit-dev-content/site/js/main.js
+++ packages/lit-dev-content/site/js/main.js
@@ -1,7 +1,6 @@
 import '@material/mwc-icon';
 import '@material/mwc-icon-button';
 import '@material/mwc-drawer';
-import './side-nav.js';
 
 // TODO(aomarks) Lazy load this.
 import 'code-sample-editor/lib/code-sample.js';
