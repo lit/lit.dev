17a4618258092b00d81ece11dbe2ad19ebefecf4
Alexander Marks <aomarks@google.com>
2020-12-21 10:46:08 -0800

    Inline drawer open click handler, it should set open before MWC elements are loaded.

--- packages/lit-dev-content/site/js/site.js
+++ packages/lit-dev-content/site/js/site.js
@@ -1,16 +1,3 @@
 import '@material/mwc-icon';
 import '@material/mwc-icon-button';
 import '@material/mwc-drawer';
-
-/**
- * Open the LHS drawer when the hamburger icon is clicked.
- */
-const enableDrawerMenuButton = () => {
-  const button = document.querySelector('.menu-button');
-  const drawer = document.querySelector('mwc-drawer');
-  button.addEventListener('click', () => {
-    drawer.open = !drawer.open;
-  });
-};
-
-window.addEventListener('DOMContentLoaded', enableDrawerMenuButton);
