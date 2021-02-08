f348ee300ab09e29288fe2455de8bc60b4e15709
Alexander Marks <aomarks@google.com>
2021-02-08 15:27:06 -0800

    Add dev mode

--- packages/lit-dev-content/.eleventy.js
+++ packages/lit-dev-content/.eleventy.js
@@ -10,10 +10,13 @@ const {
 } = require('lit-dev-tools/lib/playground-plugin/plugin.js');
 const htmlMinifier = require('html-minifier');
 const CleanCSS = require('clean-css');
+const fs = require('fs/promises');
 
 // Use the same slugify as 11ty for markdownItAnchor. It's similar to Jekyll,
 // and preserves the existing URL fragments
 const slugify = (s) => slugifyLib(s, {lower: true});
+
+const DEV = process.env.ELEVENTY_ENV === 'dev';
 
 module.exports = function (eleventyConfig) {
   // https://github.com/JordanShurmer/eleventy-plugin-toc#readme
@@ -24,9 +27,14 @@ module.exports = function (eleventyConfig) {
   });
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPlugin(playgroundPlugin);
-  eleventyConfig.addPassthroughCopy('site/css');
+  if (!DEV) {
+    // In dev mode, we symlink directly to the source CSS.
+    eleventyConfig.addPassthroughCopy('site/css');
+  }
+  // Note we don't want codemirror.css in css/ because in dev mode it's a
+  // symlink, and we don't want to accidentally copy this file into site/css.
   eleventyConfig.addPassthroughCopy({
-    'node_modules/codemirror/lib/codemirror.css': './css/codemirror.css',
+    'node_modules/codemirror/lib/codemirror.css': './codemirror.css',
   });
   eleventyConfig.addPassthroughCopy('site/images/**/*');
   eleventyConfig.addPassthroughCopy('api/**/*');
@@ -96,7 +104,7 @@ ${content}
   });
 
   eleventyConfig.addTransform('htmlMinify', function (content, outputPath) {
-    if (!outputPath.endsWith('.html')) {
+    if (DEV || !outputPath.endsWith('.html')) {
       return content;
     }
     const minified = htmlMinifier.minify(content, {
@@ -109,12 +117,49 @@ ${content}
 
   // https://www.11ty.dev/docs/quicktips/inline-css/
   eleventyConfig.addFilter('cssmin', function (code) {
+    if (DEV) {
+      return code;
+    }
     const result = new CleanCSS({}).minify(code);
     return result.styles;
   });
+  if (DEV) {
+    eleventyConfig.on('afterBuild', async () => {
+      await Promise.all([
+        // Symlink site/css -> _dev/css. We do this in dev mode instead of
+        // addPassthroughCopy() so that changes are reflected immediately,
+        // instead of triggering an Eleventy build.
+        symlinkForce(
+          path.join(__dirname, 'site', 'css'),
+          path.join(__dirname, '_dev', 'css')
+        ),
+        // Symlink lib -> _dev/lib. This lets us directly reference tsc outputs
+        // in dev mode, instead of the Rollup bundles we use for production.
+        symlinkForce(
+          path.join(__dirname, 'lib'),
+          path.join(__dirname, '_dev', 'lib')
+        ),
+      ]);
+    });
+  }
 
   return {
-    dir: {input: 'site', output: '_site'},
+    dir: {input: 'site', output: DEV ? '_dev' : '_site'},
     htmlTemplateEngine: 'njk',
   };
+};
+
+const unlinkIfExists = async (path) => {
+  try {
+    await fs.unlink(path);
+  } catch (e) {
+    if (e.code !== 'ENOENT') {
+      throw e;
+    }
+  }
+};
+
+const symlinkForce = async (target, path) => {
+  await unlinkIfExists(path);
+  await fs.symlink(target, path);
 };
