e3152d5787df1ed818f553407a7b738bb7ee745b
Alexander Marks <aomarks@google.com>
2021-02-19 14:13:37 -0800

    Prevent @web/dev-server from injecting watch script into code samples (#168)

---
+++ packages/lit-dev-content/web-dev-server.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  plugins: [
+    {
+      // When we're using web-dev-server's --watch mode, we don't want our
+      // playground project HTML files to get the injected web socket reload
+      // script tag. This plugin reverses that transformation just for those
+      // files. See https://github.com/modernweb-dev/web/issues/761 for a
+      // feature request to make this easier.
+      name: 'remove-injected-watch-script',
+      transform(ctx) {
+        if (ctx.url.startsWith('/samples/')) {
+          return {
+            body: ctx.body.replace(
+              /<!-- injected by web-dev-server.*<\/script>/gs,
+              ''
+            ),
+          };
+        }
+      },
+    },
+  ],
+};
