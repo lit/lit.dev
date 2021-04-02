9e4420ddf4c06a83bab355b26a5e6fbbf4ed3f64
Steven Orvell <sorvell@google.com>
2021-04-02 15:51:39 -0700

    Fix link

--- packages/lit-dev-content/samples/home/with-lit.ts
+++ packages/lit-dev-content/samples/home/with-lit.ts
@@ -3,14 +3,13 @@ import {customElement, property} from 'lit/decorators.js';
 
 @customElement('with-lit')
 export class WithLit extends LitElement {
-  static styles = css`:host {
-    font-family: sans-serif;
-    border: 1px dotted #333; }`;
+  static styles = css`:host { border: 1px dotted #333; }`;
 
   @property()
   action = '@Build';
 
   render() {
-    return html`<figure>${this.action}WithLit</figure>`;
+    return html`<a href=//twitter.com/buildwithlit target=_blank>
+        <figure>${this.action}WithLit</figure></a>`;
   }
 }
