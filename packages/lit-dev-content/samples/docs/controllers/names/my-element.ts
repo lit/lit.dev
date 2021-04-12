7a4e910ec745e578046d07cc1729e26811658183
Steven Orvell <sorvell@google.com>
2021-04-12 13:13:29 -0700

    Change controllers example
    Replaces Forex controller (which required a paid API key) to a more generic name controller that uses json-generator.com.

---
+++ packages/lit-dev-content/samples/docs/controllers/names/my-element.ts
@@ -0,0 +1,34 @@
+import {LitElement, html} from 'lit';
+import {customElement} from 'lit/decorators.js';
+import {NamesController, Kind, NamesResult} from './names-controller.js';
+
+@customElement('my-element')
+export class MyElement extends LitElement {
+
+  private namesController = new NamesController(this);
+
+  render() {
+    return html`Kind: <select @change=${this._kindChange}>
+      ${this.namesController.kinds.map(
+        (k) => html`<option value=${k}>${k}</option>`)
+      }
+    </select>
+    ${this.namesController.render({
+      complete: (result: NamesResult) => {
+        return html`
+          <p>List of ${this.namesController.kind}</p>
+          <ul>${result.map(i => html`<li>${i.value}</li>`)}
+          </ul>
+        `;
+      },
+      initial: () => html`<p>Choose a kind of names...</p>`,
+      pending: () => html`<p>Loading...</p>`,
+      error: (e: any) => html`<p>Error: ${e}</p>`
+    })}`;
+  }
+
+  private _kindChange(e: Event) {
+   this.namesController.kind = (e.target! as HTMLSelectElement).value as Kind;
+   this.requestUpdate();
+  }
+}
---
+++ packages/lit-dev-content/samples/docs/controllers/names/names-controller.ts
@@ -0,0 +1,53 @@
+import {ReactiveControllerHost} from 'lit';
+import {initialState, StatusRenderer, Task} from '@lit-labs/task';
+
+export type NamesResult = Array<{value: string}>;
+
+interface NamesError {
+  error: string;
+}
+
+export const kinds = ['', 'cities', 'countries', 'states', 'streets', 'error'] as const;
+
+export type Kind = typeof kinds[number];
+
+export class NamesController {
+
+  private task!: Task;
+  kinds = kinds;
+  kind: Kind = '';
+  value?: string[];
+  baseUrl = 'https://next.json-generator.com/api/json/get/';
+  kindUrlMap = {
+    '': '',
+    'cities': 'VyfXnFpH5',
+    'countries': 'Vk0bnY6B9',
+    'states': '4J5N3tTH9',
+    'streets': 'NybqntTr5',
+    // Inserted to demo an error state.
+    'error': ''
+  }
+
+  constructor(host: ReactiveControllerHost) {
+    this.task = new Task<[string], NamesResult>(host,
+      async ([kind]: [Kind]) => {
+        if (kind == null || kind.trim() === '') {
+          return initialState;
+        }
+        const response = await fetch(`${this.baseUrl}${this.kindUrlMap[kind]}`);
+        const result = await response.json() as NamesResult | NamesError;
+        const error = (result as NamesError).error;
+        if (error !== undefined) {
+          throw new Error(error);
+        }
+        return result as NamesResult;
+      },
+      () => [this.kind]
+    );
+  }
+
+  render(renderFunctions: StatusRenderer<NamesResult>) {
+    return this.task.render(renderFunctions);
+  }
+
+}
