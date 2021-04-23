1391f181a9a4d8375e0a2909c8b346cd7468431d
Arthur Evans <arthure@google.com>
2021-04-23 08:30:18 -0700

    Add slack redirect, update Slack & twitter links. (#341)

---
+++ packages/lit-dev-server/src/redirects.ts
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+export const pageRedirects  = new Map(
+  [
+    ['/slack-invite/', 'https://join.slack.com/t/lit-and-friends/shared_invite/zt-llwznvsy-LZwT13R66gOgnrg12PUGqw'],
+  ]
+)
