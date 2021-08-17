15607696df829f6ee368f72841152b5514af399b
Andrew Jakubowicz <ajakubowicz@google.com>
2021-08-17 13:39:03 -0700

    We just cannot use the exclude-internal filter with the redirect without timeout.

--- packages/lit-dev-server/src/redirects.ts
+++ packages/lit-dev-server/src/redirects.ts
@@ -4,12 +4,6 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-/**
- * Any redirects here to an external domain need to be added as exclusions in
- * the `check-links:external` test in the root `package.json`. Otherwise the
- * test queues these external domains for broken link checking crawling those
- * domains and timing out the test.
- */
 export const pageRedirects  = new Map(
   [
     ['/slack-invite/', 'https://join.slack.com/t/lit-and-friends/shared_invite/zt-llwznvsy-LZwT13R66gOgnrg12PUGqw'],
