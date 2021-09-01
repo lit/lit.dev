2f73ca47e36d3971d384751632acb1bb942e2c38
Alexander Marks <aomarks@google.com>
2021-09-01 07:41:48 -0700

    Automatically add trailing slash to redirect keys

--- packages/lit-dev-server/src/redirects.ts
+++ packages/lit-dev-server/src/redirects.ts
@@ -4,8 +4,12 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-export const pageRedirects  = new Map(
-  [
-    ['/slack-invite/', 'https://join.slack.com/t/lit-and-friends/shared_invite/zt-llwznvsy-LZwT13R66gOgnrg12PUGqw'],
-  ]
-)
+// prettier-ignore
+export const pageRedirects = new Map([
+  ['/slack-invite', 'https://join.slack.com/t/lit-and-friends/shared_invite/zt-llwznvsy-LZwT13R66gOgnrg12PUGqw'],
+].map(([path, redir]) => [
+  // Trailing slashes are required because this redirect map is consulted after
+  // standard lit.dev path canonicalization.
+  path.match(/\/[^\/\.]+$/) ? path + '/' : path,
+  redir,
+]));
