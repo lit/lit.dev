c90e4e39aa7317380f24e8bfe55a864096c67df5
Alexander Marks <aomarks@google.com>
2021-09-01 10:52:29 -0700

    Redirects for dev mode short links (#465)
    Adds a https://lit.dev/msg/<code> redirect for all warnings logged by Lit's dev mode.
    
    Half of lit/lit#2078. Other half is at lit/lit#2119.

--- packages/lit-dev-server/src/redirects.ts
+++ packages/lit-dev-server/src/redirects.ts
@@ -4,8 +4,26 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-export const pageRedirects  = new Map(
-  [
-    ['/slack-invite/', 'https://join.slack.com/t/lit-and-friends/shared_invite/zt-llwznvsy-LZwT13R66gOgnrg12PUGqw'],
-  ]
-)
+// prettier-ignore
+export const pageRedirects = new Map([
+  ['/slack-invite', 'https://join.slack.com/t/lit-and-friends/shared_invite/zt-llwznvsy-LZwT13R66gOgnrg12PUGqw'],
+
+  ['/msg/dev-mode',                  '/docs/tools/development/#development-and-production-builds'],
+  // TODO(sorvell) https://github.com/lit/lit.dev/issues/455
+  ['/msg/multiple-versions',         '/docs/tools/requirements/'],
+  ['/msg/polyfill-support-missing',  '/docs/tools/requirements/#polyfills'],
+  // TODO(sorvell) https://github.com/lit/lit.dev/issues/462
+  ['/msg/class-field-shadowing',     '/docs/components/properties/#declare'],
+  // TODO(aomarks) Should we add something specifically about this issue?
+  ['/msg/change-in-update',          '/docs/components/properties/#when-properties-change'],
+  ['/msg/deprecated-import-path',    '/docs/releases/upgrade/#update-packages-and-import-paths'],
+  ['/msg/removed-api',               '/docs/releases/upgrade/#litelement'],
+  ['/msg/renamed-api',               '/docs/releases/upgrade/#update-to-renamed-apis'],
+  ['/msg/undefined-attribute-value', '/docs/releases/upgrade/#litelement'],
+  ['/msg/request-update-promise',    '/docs/releases/upgrade/#litelement'],
+].map(([path, redir]) => [
+  // Trailing slashes are required because this redirect map is consulted after
+  // standard lit.dev path canonicalization.
+  path.match(/\/[^\/\.]+$/) ? path + '/' : path,
+  redir,
+]));
