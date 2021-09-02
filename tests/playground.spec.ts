9c56a7e81c2bdf3a5e05683534725547356d32c0
Andrew Jakubowicz <spyr1014@gmail.com>
2021-09-01 17:09:12 -0700

    Add lit.dev integration testing and cache github actions (#463)
    Add integration tests that run in CI.
    
    This change tests:
     - Some attributes on the home page.
     - Site Search basic usage.
     - Simple playground edit causes the preview to update.
    
    * Cache ~/.npm build if package-lock hasn't changed in Github actions
    
    Co-authored-by: Andrew Jakubowicz <ajakubowicz@google.com>

---
+++ tests/playground.spec.ts
@@ -0,0 +1,66 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {test, expect} from '@playwright/test';
+
+test.describe('Playground', () => {
+  test('default example is simple-greeting.ts', async ({page}) => {
+    await page.goto(`/playground`);
+
+    await page.waitForSelector(
+      'playground-preview [part="preview-loading-indicator"][aria-hidden="true"]'
+    );
+
+    const greetingExample = page.locator(
+      '#exampleContent > div:nth-child(1) > ul > li:nth-child(1)'
+    );
+    await expect(greetingExample).toHaveClass('exampleItem active');
+
+    const codeEditor = page.locator('playground-code-editor #focusContainer');
+    expect(
+      (await codeEditor.textContent()).includes(
+        `@customElement('simple-greeting')`
+      )
+    ).toBe(true);
+
+    const playgroundPreviewFrame = await (
+      await page.locator('playground-preview iframe').elementHandle()
+    ).contentFrame();
+    await expect(
+      playgroundPreviewFrame.locator('simple-greeting p')
+    ).toHaveText('Hello, World!');
+    await expect(playgroundPreviewFrame.locator('simple-greeting p')).toHaveCSS(
+      'color',
+      'rgb(0, 0, 255)'
+    );
+  });
+
+  test('updating the example code updates the preview', async ({page}) => {
+    await page.goto(`/playground`);
+
+    // Double click text=blue
+    await page.dblclick('text=blue');
+
+    // Change the text to red
+    await page.keyboard.type('red');
+
+    // Wait for iframe to reload
+    await page.waitForSelector(
+      'playground-preview [part="preview-loading-indicator"][aria-hidden="true"]'
+    );
+
+    const playgroundPreviewFrame = await (
+      await page.locator('playground-preview iframe').elementHandle()
+    ).contentFrame();
+    await expect(
+      playgroundPreviewFrame.locator('simple-greeting p')
+    ).toHaveText('Hello, World!');
+    await expect(playgroundPreviewFrame.locator('simple-greeting p')).toHaveCSS(
+      'color',
+      'rgb(255, 0, 0)'
+    );
+  });
+});
