a8acd7d413fd7b06e8e05041bf03056df5bc3098
Alexander Marks <aomarks@google.com>
2021-09-07 17:08:20 -0700

    Tweak alignment of open in playground button

--- packages/lit-dev-content/src/components/litdev-example-controls.ts
+++ packages/lit-dev-content/src/components/litdev-example-controls.ts
@@ -15,9 +15,6 @@ export class LitDevExampleControls extends LitElement {
   static override styles = css`
     :host {
       display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: center;
       margin-left: auto;
     }
 
@@ -49,7 +46,7 @@ export class LitDevExampleControls extends LitElement {
         href="/playground/#sample=${this.project}"
       >
         <!-- Source: https://material.io/resources/icons/?icon=launch&style=baseline -->
-        <svg width="22px" height="22px" viewBox="0 0 24 24" fill="#5f5f5f">
+        <svg width="22px" height="22px" viewBox="0 2 24 24" fill="#5f5f5f">
           <path
             d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"
           />
