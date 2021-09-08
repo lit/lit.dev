e4eba74f643cc9f18fa295eff520b9a515131400
Alexander Marks <aomarks@google.com>
2021-09-08 08:02:06 -0700

    Don't show open-in-playground link when no project

--- packages/lit-dev-content/src/components/litdev-example-controls.ts
+++ packages/lit-dev-content/src/components/litdev-example-controls.ts
@@ -5,6 +5,7 @@
  */
 
 import {LitElement, html, css, customElement, property} from 'lit-element';
+import {nothing} from 'lit-html';
 import './litdev-typescript-switch.js';
 
 /**
@@ -40,19 +41,21 @@ export class LitDevExampleControls extends LitElement {
   override render() {
     return html`
       <litdev-typescript-switch></litdev-typescript-switch>
-      <a
-        id="openInPlayground"
-        title="Open this example in the playground"
-        target="_blank"
-        href="/playground/#sample=${this.project}"
-      >
-        <!-- Source: https://material.io/resources/icons/?icon=launch&style=baseline -->
-        <svg width="22px" height="22px" viewBox="0 0 24 24" fill="#5f5f5f">
-          <path
-            d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"
-          />
-        </svg>
-      </a>
+      ${this.project
+        ? html`<a
+            id="openInPlayground"
+            title="Open this example in the playground"
+            target="_blank"
+            href="/playground/#sample=${this.project}"
+          >
+            <!-- Source: https://material.io/resources/icons/?icon=launch&style=baseline -->
+            <svg width="22px" height="22px" viewBox="0 0 24 24" fill="#5f5f5f">
+              <path
+                d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"
+              />
+            </svg>
+          </a>`
+        : nothing}
     `;
   }
 }
