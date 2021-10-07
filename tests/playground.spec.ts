8858290979e2ce56aa25a452fbe183b2ee08b8df
Alexander Marks <aomarks@google.com>
2021-10-06 17:56:13 -0700

    Bump playground version (#543)
    Brings in all the changes from https://github.com/google/playground-elements/releases/tag/v0.14.3
    
    Also updated integration tests to account for playground changes.

--- tests/playground.spec.ts
+++ tests/playground.spec.ts
@@ -71,6 +71,23 @@ test.describe('Playground', () => {
 });
 
 async function waitForPlaygroundPreviewToLoad(page: Page) {
+  // We could get a series of iframe reloads, e.g. if we're typing multiple
+  // characters, then there could be enough time for multiple previews to get
+  // queued up inbetween each keystroke. Assume we've settled when we haven't
+  // had an iframe load event for some period of time.
+  const iframe = await page.waitForSelector('playground-preview iframe');
+  await page.waitForFunction(async (iframe) => {
+    const settleTime = 1000;
+    await new Promise<void>((resolve) => {
+      let timer = setTimeout(resolve, settleTime);
+      iframe.addEventListener('load', () => {
+        clearTimeout(timer);
+        timer = setTimeout(resolve, settleTime);
+      });
+    });
+    return true;
+  }, iframe);
+  // Wait for the loading indicator to stop for screenshots.
   await page.waitForSelector(
     'playground-preview [part="preview-loading-indicator"][aria-hidden="true"]'
   );
