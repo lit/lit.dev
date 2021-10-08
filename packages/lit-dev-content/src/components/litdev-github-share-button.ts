206121ebec304ab3549a5ec18f4ffa89a2ea8282
Alexander Marks <aomarks@google.com>
2021-10-08 16:13:02 -0700

    Update share button to actually write gists

--- packages/lit-dev-content/src/components/litdev-github-share-button.ts
+++ packages/lit-dev-content/src/components/litdev-github-share-button.ts
@@ -7,12 +7,28 @@
 import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import {signInToGithub} from '../github/github-signin.js';
+import {createGist} from '../github/github-gists.js';
+
+import type {GistFiles} from '../github/github-gists.js';
+import type {SampleFile} from 'playground-elements/shared/worker-api.js';
+
+/**
+ * An in-memory cache of the GitHub authentication tokens associated with each
+ * instance of a share button. This allows the user to authenticate only once
+ * per page load, instead of on each save.
+ *
+ * By using a WeakMap instead of a class instance property, we make it much more
+ * difficult for code outside of this module to directly access tokens.
+ *
+ * (It's not expected to usually have more than one instance of a share button,
+ * but it could happen e.g. in testing.)
+ */
+const tokenCache = new WeakMap<LitDevGitHubShareButton, string>();
 
 /**
  * A button that shares a Playground project as a GitHub gist. If the user isn't
  * signed into GitHub yet, they are prompted to do so.
  *
- * // TODO(aomarks) Implement actually sharing. This is just an early WIP.
  * // TODO(aomarks) Show a scrim and some indication about what is happening
  * //               while the popup is open.
  * // TODO(aomarks) Style this button
@@ -32,20 +48,50 @@ export class LitDevGitHubShareButton extends LitElement {
   @property()
   authorizeUrl?: string;
 
+  /**
+   * Base URL for the GitHub API.
+   */
+  @property()
+  githubApiUrl?: string;
+
+  /**
+   * A function to allow this component to access the project upon save.
+   */
+  getProjectFiles?: () => SampleFile[] | undefined;
+
   override render() {
     return html`<button @click=${this._onClick}>Share with GitHub</button>`;
   }
 
   private async _onClick() {
-    if (!this.clientId || !this.authorizeUrl) {
+    const projectFiles = this.getProjectFiles?.();
+    if (
+      !this.githubApiUrl ||
+      !this.clientId ||
+      !this.authorizeUrl ||
+      !projectFiles
+    ) {
       throw new Error('Missing required properties');
     }
-    // TODO(aomarks) Prevent multiple signups from being queued.
-    const accessToken = await signInToGithub({
-      clientId: this.clientId,
-      authorizeUrl: this.authorizeUrl,
+
+    let token = tokenCache.get(this);
+    if (token === undefined) {
+      token = await signInToGithub({
+        clientId: this.clientId,
+        authorizeUrl: this.authorizeUrl,
+      });
+      tokenCache.set(this, token);
+    }
+
+    const gistFiles: GistFiles = Object.fromEntries(
+      projectFiles.map((file) => [file.name, {content: file.content}])
+    );
+
+    const gistId = await createGist(gistFiles, {
+      apiBaseUrl: this.githubApiUrl,
+      token,
     });
-    alert(accessToken);
+    this.dispatchEvent(new CustomEvent('gist-created', {detail: {gistId}}));
   }
 }
 
