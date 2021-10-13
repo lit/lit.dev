38a8a96fb54b4c860604851711263f29ee3af670
Alexander Marks <aomarks@google.com>
2021-10-13 13:32:28 -0700

    Unified base64 + gist share flyout (#558)
    Creates a new unified flyout for the "Share" button (still behind the ?mods=gists flag).
    
    Part of #533

--- packages/lit-dev-content/src/components/copy-button.ts
+++ packages/lit-dev-content/src/components/copy-button.ts
@@ -6,6 +6,8 @@
 
 import {LitElement, html, css} from 'lit';
 import {property} from 'lit/decorators.js';
+import {copyIcon} from '../icons/copy-icon.js';
+
 import '@material/mwc-icon-button';
 
 /**
@@ -28,12 +30,7 @@ export class CopyButton extends LitElement {
       @click=${this._click}
       label="Copy to clipboard"
     >
-      <!-- Source: https://material.io/resources/icons/?icon=content_copy&style=baseline -->
-      <svg width="24" height="24" viewBox="0 0 24 24">
-        <path
-          d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"
-        />
-      </svg>
+      ${copyIcon}
     </mwc-icon-button>`;
   }
 
