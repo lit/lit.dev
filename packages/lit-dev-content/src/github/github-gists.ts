c3729e982dc2e334a0028f0384a623dc7a8a71b6
Alexander Marks <aomarks@google.com>
2021-10-15 08:16:34 -0700

    Add function for updating a gist

--- packages/lit-dev-content/src/github/github-gists.ts
+++ packages/lit-dev-content/src/github/github-gists.ts
@@ -27,18 +27,18 @@ export interface GistFile {
  * Get an existing GitHub gist.
  */
 export const getGist = async (
-  id: string,
+  gistId: string,
   opts: GitHubApiOptions
 ): Promise<Gist> => {
   // https://docs.github.com/en/rest/reference/gists#get-a-gist
-  const url = new URL(`/gists/${id}`, opts.apiBaseUrl).href;
+  const url = new URL(`/gists/${gistId}`, opts.apiBaseUrl).href;
   const res = await fetch(url, {
     headers: {
       accept: JSON_GITHUB_API_V3,
     },
   });
   if (!res.ok) {
-    throw new Error(`HTTP ${res.status} error getting gist ${id}`);
+    throw new Error(`HTTP ${res.status} error getting gist ${gistId}`);
   }
   return res.json() as Promise<Gist>;
 };
@@ -65,4 +65,31 @@ export const createGist = async (
   }
   const gist = (await res.json()) as Gist;
   return gist.id;
+};
+
+/**
+ * Create a new revision of an existing GitHub gist.
+ */
+export const updateGist = async (
+  gistId: string,
+  files: GistFiles,
+  opts: AuthenticatedGitHubApiOptions
+): Promise<Gist> => {
+  // Create a new revision of an existing GitHub gist.
+  const url = new URL(`/gists/${gistId}`, opts.apiBaseUrl).href;
+  const res = await fetch(url, {
+    method: 'PATCH',
+    headers: {
+      accept: JSON_GITHUB_API_V3,
+      authorization: `token ${opts.token}`,
+    },
+    body: JSON.stringify({
+      gist_id: gistId,
+      files,
+    }),
+  });
+  if (!res.ok) {
+    throw new Error(`HTTP ${res.status} error updating gist`);
+  }
+  return res.json() as Promise<Gist>;
 };
