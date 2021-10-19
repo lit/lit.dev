lit/lit.dev

commit a8f83a2ac51cc5f77b061c7f9cc0d360b809a004

Refactor icon buttons into common component (#556)
- Creates a new `<litdev-icon-button>` component.
- Updates the new Playground share button and the existing Tutorial solve & reset buttons to use it.
- Creates a new `<litdev-playground-download-button>` that uses the new shared button, and moves the tarball functionality out of the main `playground.ts` file into this component.
- Renames `<litdev-github-share-button>` to `<litdev-playground-share-button>`. I'm intending to make this component support both styles of sharing.
- Adds a screenshot test for the Tutorial page, to confirm the styles are the same after the refactor.

packages/lit-dev-content/src/components/litdev-playground-download-button.ts
@@ -1,0 +1,67 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {LitElement, html, css} from 'lit';
+import {customElement} from 'lit/decorators.js';
+import {downloadIcon} from '../icons/download-icon.js';
+import Tar from 'tarts';
+
+import './litdev-icon-button.js';
+
+import type {SampleFile} from 'playground-elements/shared/worker-api.js';
+
+/**
+ * A button that prompts the user to download a playground project as a tarball.
+ *
+ * // TODO(aomarks) Support zip files.
+ */
+@customElement('litdev-playground-download-button')
+export class LitDevPlaygroundDownloadButton extends LitElement {
+  static styles = css`
+    litdev-icon-button:hover {
+      background: blue;
+    }
+  `;
+
+  /**
+   * A function to allow this component to access the project upon download.
+   */
+  getProjectFiles?: () => SampleFile[] | undefined;
+
+  override render() {
+    return html`
+      <litdev-icon-button @click=${this._onClick}>
+        ${downloadIcon} Download
+      </litdev-icon-button>
+    `;
+  }
+
+  private async _onClick() {
+    const projectFiles = this.getProjectFiles?.();
+    if (!projectFiles) {
+      throw new Error('Missing required properties');
+    }
+    if (projectFiles.length === 0) {
+      // TODO(aomarks) The button should just be disabled in this case.
+      throw new Error("Can't download an empty project");
+    }
+    const tarFiles = Object.entries(projectFiles).map(([name, {content}]) => ({
+      name,
+      content: content ?? '',
+    }));
+    const tar = Tar(tarFiles);
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(new Blob([tar], {type: 'application/tar'}));
+    a.download = 'lit-playground.tar';
+    a.click();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'litdev-playground-download-button': LitDevPlaygroundDownloadButton;
+  }
+}

.github/workflows/generate-goldens.yml
@@ -23,6 +23,9 @@ jobs:
       - name: NPM install
         run: npm ci
 
+      - name: Install playwright deps
+        run: npm run test:integration:install-deps
+
       - name: Lerna bootstrap
         run: npm run bootstrap
 

packages/lit-dev-content/site/css/mods.css
@@ -23,9 +23,12 @@
   --playground-code-callee-color: #dcdcaa;
 }
 
-litdev-github-share-button {
+litdev-playground-share-button {
   display: none;
 }
-.gists litdev-github-share-button {
+.gists litdev-playground-share-button {
   display: inline-block;
+}
+.gists #shareButton {
+  display: none;
 }

packages/lit-dev-content/site/css/tutorial.css
@@ -110,28 +110,16 @@ litdev-tutorial {
   justify-content: center;
 }
 
-#tutorialFooter > button {
-  color: white;
-  background: transparent;
-  font-size: 1em;
-  border: 1px solid white;
-  margin-bottom: 0.5em;
+#tutorialFooter > litdev-icon-button {
   padding: 0.3em 0.6em;
-  text-align: center;
   margin-right: 1em;
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-  outline-color: var(--color-dark-cyan);
-  font-family: "Open Sans", sans-serif;
 }
 
-#tutorialFooter > button > svg {
+#tutorialFooter > litdev-icon-button > svg {
   margin-right: 0.5em;
 }
 
-#tutorialFooter > button:hover {
-  text-decoration: none;
+#tutorialFooter > litdev-icon-button:hover {
   background: #2a2a2a;
 }
 

packages/lit-dev-content/site/playground/index.html
@@ -40,18 +40,8 @@ title: Playground
 
 <div id="content">
   <div id="actionBar">
-    <button id="downloadButton" class="outlineButton">
-      <svg width="24" height="24" viewBox="0 0 8.9403505 7.7663145"
-           stroke="currentcolor" stroke-width="0.5" fill="none"
-           aria-hidden="true">
-        <g transform="translate(-60.514912,-89.035921)">
-          <path d="m 60.779495,92.719876 v 3.817776 h 8.411184 v -3.768603" />
-          <path d="M 64.978456,94.503808 V 89.035921" />
-          <path d="M 66.807058,92.468875 64.978456,94.470831 63.117735,92.61011" />
-        </g>
-      </svg>
-      Download
-    </button>
+    <litdev-playground-download-button>
+    </litdev-playground-download-button>
 
     <button id="shareButton" class="outlineButton">
       <svg width="24" height="24" viewBox="0 0 7.9368957 14.252527"
@@ -66,11 +56,11 @@ title: Playground
       Share
     </button>
 
-    <litdev-github-share-button
+    <litdev-playground-share-button
       clientId="{{ env.GITHUB_CLIENT_ID }}"
       authorizeUrl="{{ env.GITHUB_AUTHORIZE_URL }}"
       githubApiUrl="{{ env.GITHUB_API_URL }}">
-    </litdev-github-share-button>
+    </litdev-playground-share-button>
   </div>
 
   <playground-project

packages/lit-dev-content/src/components/litdev-icon-button.ts
@@ -1,0 +1,71 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {LitElement, html, css} from 'lit';
+import {customElement} from 'lit/decorators.js';
+
+/**
+ * A button with lit.dev styling.
+ *
+ * If an svg element is slotted as the first child, its size and margin is set
+ * automatically.
+ */
+@customElement('litdev-icon-button')
+export class LitDevIconButton extends LitElement {
+  static override styles = css`
+    :host {
+      border: 1px solid currentcolor;
+      box-sizing: border-box;
+      cursor: pointer;
+      display: inline-block;
+      font-family: 'Open Sans', sans-serif;
+      font-size: 16px;
+      padding: 5px 8px;
+    }
+
+    :host(:hover) {
+      background-color: rgba(0, 0, 0, 0.25);
+    }
+
+    button {
+      align-items: center;
+      background: transparent;
+      border: none;
+      color: inherit;
+      cursor: inherit;
+      display: flex;
+      font-family: inherit;
+      font-size: inherit;
+      margin: 0;
+      padding: 0;
+    }
+
+    slot {
+      display: flex;
+      align-items: center;
+    }
+
+    ::slotted(svg:first-child) {
+      height: 1.5em;
+      margin-right: 1em;
+      width: 1.5em;
+    }
+  `;
+
+  override render() {
+    return html`
+      <button>
+        <slot></slot>
+      </button>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'litdev-icon-button': LitDevIconButton;
+  }
+}

packages/lit-dev-content/src/components/litdev-playground-share-button.ts
@@ -4,10 +4,14 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import {LitElement, html} from 'lit';
+import {LitElement, html, css} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import {signInToGithub} from '../github/github-signin.js';
 import {createGist} from '../github/github-gists.js';
+
+import {shareIcon} from '../icons/share-icon.js';
+
+import './litdev-icon-button.js';
 
 import type {GistFiles} from '../github/github-gists.js';
 import type {SampleFile} from 'playground-elements/shared/worker-api.js';
@@ -23,7 +27,7 @@ import type {SampleFile} from 'playground-elements/shared/worker-api.js';
  * (It's not expected to usually have more than one instance of a share button,
  * but it could happen e.g. in testing.)
  */
-const tokenCache = new WeakMap<LitDevGitHubShareButton, string>();
+const tokenCache = new WeakMap<LitDevPlaygroundShareButton, string>();
 
 /**
  * A button that shares a Playground project as a GitHub gist. If the user isn't
@@ -31,10 +35,15 @@ const tokenCache = new WeakMap<LitDevGitHubShareButton, string>();
  *
  * // TODO(aomarks) Show a scrim and some indication about what is happening
  * //               while the popup is open.
- * // TODO(aomarks) Style this button
  */
-@customElement('litdev-github-share-button')
-export class LitDevGitHubShareButton extends LitElement {
+@customElement('litdev-playground-share-button')
+export class LitDevPlaygroundShareButton extends LitElement {
+  static styles = css`
+    litdev-icon-button:hover {
+      background: blue;
+    }
+  `;
+
   /**
    * GitHub OAuth App client ID. Generated when a GitHub OAuth App is first
    * created.
@@ -60,7 +69,11 @@ export class LitDevGitHubShareButton extends LitElement {
   getProjectFiles?: () => SampleFile[] | undefined;
 
   override render() {
-    return html`<button @click=${this._onClick}>Share with GitHub</button>`;
+    return html`
+      <litdev-icon-button @click=${this._onClick}>
+        ${shareIcon} Share
+      </litdev-icon-button>
+    `;
   }
 
   private async _onClick() {
@@ -110,6 +123,6 @@ declare global {
     'gist-created': CustomEvent<{gistId: string}>;
   }
   interface HTMLElementTagNameMap {
-    'litdev-github-share-button': LitDevGitHubShareButton;
+    'litdev-playground-share-button': LitDevPlaygroundShareButton;
   }
 }

packages/lit-dev-content/src/components/litdev-tutorial.ts
@@ -14,9 +14,13 @@ import {
   getCodeLanguagePreference,
   CODE_LANGUAGE_CHANGE,
 } from '../code-language-preference.js';
+import {solveIcon} from '../icons/solve-icon.js';
+import {resetIcon} from '../icons/reset-icon.js';
+
 import '@material/mwc-icon-button';
 import './litdev-example-controls.js';
 import './litdev-playground-change-guard.js';
+import './litdev-icon-button.js';
 
 interface ExpandedTutorialStep extends TutorialStep {
   idx: number;
@@ -124,40 +128,13 @@ export class LitDevTutorial extends LitElement {
         ${unsafeHTML(this._html)}
 
         <div id="tutorialFooter">
-          <button @click=${this._onClickSolve}>
-            <!-- Source: https://material.io/resources/icons/?icon=auto_fix_high -->
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="currentcolor"
-              aria-hidden="true"
-            >
-              <path
-                d="M7.5 5.6L10 7 8.6 4.5 10 2 7.5 3.4 5 2l1.4 2.5L5 7zm12 9.8L17 14l1.4 2.5L17 19l2.5-1.4L22 19l-1.4-2.5L22 14zM22 2l-2.5 1.4L17 2l1.4 2.5L17 7l2.5-1.4L22 7l-1.4-2.5zm-7.63 5.29c-.39-.39-1.02-.39-1.41 0L1.29 18.96c-.39.39-.39 1.02 0 1.41l2.34 2.34c.39.39 1.02.39 1.41 0L16.7 11.05c.39-.39.39-1.02 0-1.41l-2.33-2.35zm-1.03 5.49l-2.12-2.12 2.44-2.44 2.12 2.12-2.44 2.44z"
-              />
-            </svg>
-            Solve
-          </button>
+          <litdev-icon-button @click=${this._onClickSolve}>
+            ${solveIcon} Solve
+          </litdev-icon-button>
 
-          <button @click=${this._onClickReset}>
-            <!-- Source: https://material.io/resources/icons/?icon=restart_alt -->
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="currentcolor"
-              aria-hidden="true"
-            >
-              <path
-                d="M12,5V2L8,6l4,4V7c3.31,0,6,2.69,6,6c0,2.97-2.17,5.43-5,5.91v2.02c3.95-0.49,7-3.85,7-7.93C20,8.58,16.42,5,12,5z"
-              />
-              <path
-                d="M6,13c0-1.65,0.67-3.15,1.76-4.24L6.34,7.34C4.9,8.79,4,10.79,4,13c0,4.08,3.05,7.44,7,7.93v-2.02 C8.17,18.43,6,15.97,6,13z"
-              />
-            </svg>
-            Reset
-          </button>
+          <litdev-icon-button @click=${this._onClickReset}>
+            ${resetIcon} Reset
+          </litdev-icon-button>
 
           <span id="nextStep">
             ${this._nextInfo

commit bcd601f11758bea2951bc7b145bd3c8c7400c46e

Fix bug with playground tarball filenames (#567)
Almost certainly a regression from https://github.com/lit/lit.dev/pull/556

Was using array indexes as filenames, instead of the actual filenames.

packages/lit-dev-content/src/components/litdev-playground-download-button.ts
@@ -48,7 +48,7 @@ export class LitDevPlaygroundDownloadButton extends LitElement {
       // TODO(aomarks) The button should just be disabled in this case.
       throw new Error("Can't download an empty project");
     }
-    const tarFiles = Object.entries(projectFiles).map(([name, {content}]) => ({
+    const tarFiles = projectFiles.map(({name, content}) => ({
       name,
       content: content ?? '',
     }));