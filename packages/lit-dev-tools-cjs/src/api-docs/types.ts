3dcc81a34ef103cd6877c0e8d897ad523a6f3c68
Alexander Marks <aomarks@google.com>
2021-11-30 08:55:31 -0800

    Add ApiDocsConfig type

--- packages/lit-dev-tools-cjs/src/api-docs/types.ts
+++ packages/lit-dev-tools-cjs/src/api-docs/types.ts
@@ -37,3 +37,90 @@ export interface Location {
 export interface ExternalLocation {
   url: string;
 }
+
+export interface ApiDocsConfig {
+  /**
+   * Git repo remote URL.
+   */
+  repo: string;
+
+  /**
+   * Git commit SHA.
+   */
+  sha: string;
+
+  /**
+   * Location where Git repo will be cloned.
+   */
+  gitDir: string;
+
+  /**
+   * Path to the tsconfig.json that owns the entrypoint modules.
+   */
+  tsConfigPath: string;
+
+  /**
+   * The directory that TypeDoc chooses as the root of this package. Unclear why
+   * this is unpredictable.
+   */
+  typedocRoot: string;
+
+  /**
+   * Entrypoint TypeScript modules for TypeDoc to analyze.
+   *
+   * The modules listed here should be the preferred modules that users should
+   * import from, because import statements will be generated using these
+   * entrypoints as the module specifier (e.g. `import {LitElement} from
+   * 'lit'`). GitHub source links will be generated pointing at the ultimate
+   * location where the symbol is concretely defined (e.g.
+   * `packages/lit-element/src/lit-element.ts`).
+   *
+   * If a directory, all .ts files within it are included.
+   */
+  entrypointModules: string[];
+
+  /**
+   * Where to write the API data that is consumed by our Eleventy template.
+   */
+  pagesOutPath: string;
+
+  /**
+   * Where to write the index from $symbol to location object.
+   */
+  symbolsOutPath: string;
+
+  /**
+   * Order that items in an API docs page will appear. Falls back to
+   * lexicographic. (Note this doesn't distinguish between pages, but we don't
+   * have any overlapping export names for now so it doesn't matter).
+   */
+  symbolOrder: string[];
+
+  /**
+   * Pages in the order they will appear in the navigation.
+   */
+  pages: Array<{
+    slug: string;
+    title: string;
+    anchorFilter?: (node: DeclarationReflection) => boolean;
+  }>;
+
+  /**
+   * Determine which generated API docs page the given TypeDoc reflection object
+   * belongs in.
+   *
+   * Note that only top-level exports of each entrypoint module are passed here
+   * (i.e. the methods of a class always go on the same page as the class).
+   */
+  pageForSymbol(node: ExtendedDeclarationReflection): string;
+
+  /**
+   * Generate a relative URL for the given location.
+   */
+  locationToUrl({page, anchor}: Location): string;
+
+  /**
+   * Maps a git-repo relative filename to an import specifier.
+   */
+  fileToImportSpecifier(filename: string): string;
+}
