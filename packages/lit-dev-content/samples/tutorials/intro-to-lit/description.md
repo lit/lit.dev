7e0e9db43c82b37c48a6b0a0cd2fec4f25badf8c
Elliott Marquez <5981958+e111077@users.noreply.github.com>
2022-01-24 15:21:30 -0800

    convert TS pipeline into json pipeline

---
+++ packages/lit-dev-content/samples/tutorials/intro-to-lit/description.md
@@ -0,0 +1,1 @@
+New to Lit? Start here to learn the ropes and make your first LitElement.
--- packages/lit-dev-content/site/tutorials/tutorials.11tydata.js
+++
@@ -1,9 +0,0 @@
-const {generateManifest} = require('../../samples/js/tutorials/utils.cjs.js');
-const {getManifest} = require('../../samples/js/tutorials/intro-to-lit/11ty-data.cjs.js');
-
-// This file is replaced in a subsequent PR. It's only here to keep the build
-// functioning for the current PR
-module.exports = async () => {
-  await generateManifest(await getManifest(), 'intro-to-lit');
-  return {};
-}
