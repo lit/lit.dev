bd7a32c4fe531a247045a1ca18a6be17eac33454
Andrew Jakubowicz <ajakubowicz@google.com>
2022-01-27 09:46:47 -0800

    Add comments and next function to the middleware

--- packages/lit-dev-server/src/middleware/notfound-middleware.ts
+++ packages/lit-dev-server/src/middleware/notfound-middleware.ts
@@ -9,11 +9,14 @@ import send from 'koa-send';
 import type Koa from 'koa';
 
 /**
- * Create a Koa middleware that serves a 404.html page on 404 status codes.
+ * Creates a Koa middleware that serves a 404.html page on 404 status codes.
  */
 export const notFoundMiddleware =
   (staticPath: string): Koa.Middleware =>
-  async (ctx) => {
+  async (ctx, next) => {
+    // Run other middleware first, so the 404 is handled last.
+    await next();
+
     if (ctx.status === 404) {
       await send(ctx, '/404/index.html', {root: staticPath});
     }
