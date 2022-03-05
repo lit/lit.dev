9c3820249d7e4898fd67c9ca1036a564f39758b2
e111077 <5981958+e111077@users.noreply.github.com>
2022-03-04 16:18:10 -0800

    add cheap check for dsd support

--- packages/lit-dev-content/src/global/dsd-polyfill.ts
+++ packages/lit-dev-content/src/global/dsd-polyfill.ts
@@ -1,5 +1,6 @@
 import {hydrateShadowRoots} from '@webcomponents/template-shadowroot/template-shadowroot.js';
 
-// Polyfill will short if the browser supports native DSD
-hydrateShadowRoots(document.body);
+if (!HTMLTemplateElement.prototype.hasOwnProperty('shadowRoot')) {
+  hydrateShadowRoots(document.body);
+};
 document.body.removeAttribute('dsd-pending');
