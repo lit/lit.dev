aed045f4488840e6937d010cbd59d5d5733462d7
Rody Davis <rody.davis.jr@gmail.com>
2022-03-24 15:23:44 -0700

    addnig base

---
+++ packages/lit-dev-content/samples/tutorials/brick-viewer/00/before/my-element.ts
@@ -0,0 +1,15 @@
+import {LitElement, html} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+
+@customElement('my-element')
+class MyElement extends LitElement {
+  @property()
+  version = 'STARTING';
+
+  render() {
+    return html`
+    <p>Welcome to the Lit tutorial!</p>
+    <p>This is the ${this.version} code.</p>
+    `;
+  }
+}
