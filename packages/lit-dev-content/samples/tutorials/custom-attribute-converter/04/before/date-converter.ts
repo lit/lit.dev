7a9d4dd455373c79bfa17e752fd639e47e3f6b4a
e111077 <5981958+e111077@users.noreply.github.com>
2022-04-12 15:26:43 -0700

    implement suggestions

--- packages/lit-dev-content/samples/tutorials/custom-attribute-converter/04/before/date-converter.ts
+++ packages/lit-dev-content/samples/tutorials/custom-attribute-converter/04/before/date-converter.ts
@@ -1,4 +1,4 @@
-import {ComplexAttributeConverter} from 'lit';
+import type {ComplexAttributeConverter} from 'lit';
 
 export const dateConverter: ComplexAttributeConverter<Date> = {
   toAttribute: (date: Date) => {
