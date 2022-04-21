2d83a440293efcdb0869c4c68cef027d0c7dbcf1
Steve Orvell <sorvell@google.com>
2022-04-21 16:56:19 -0700

    [tutorial] Build a Tooltip element (#740)
    Adds Tooltip tutorial.

---
+++ packages/lit-dev-content/samples/tutorials/tooltip/03/before/simple-tooltip.ts
@@ -0,0 +1,36 @@
+import {html, css, LitElement} from 'lit';
+import {customElement} from 'lit/decorators.js';
+
+@customElement('simple-tooltip')
+export class SimpleTooltip extends LitElement {
+
+  static styles = css`
+    :host {
+      display: inline-block;
+      position: fixed;
+      padding: 4px;
+      border: 1px solid darkgray;
+      border-radius: 4px;
+      background: #ccc;
+      pointer-events: none;
+    }
+  `;
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.hide();
+  }
+
+  render() {
+    return html`<slot></slot>`;
+  }
+
+  show = () => {
+    this.style.cssText = '';
+  };
+
+  hide = () => {
+    this.style.display = 'none';
+  };
+
+}
