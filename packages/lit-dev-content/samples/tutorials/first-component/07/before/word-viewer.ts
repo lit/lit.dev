03e45026d6d1c0da0f6504ce2de1ecedbd245d5b
Andrew Jakubowicz <ajakubowicz@google.com>
2022-04-20 21:43:40 -0700

    Step 7

---
+++ packages/lit-dev-content/samples/tutorials/first-component/07/before/word-viewer.ts
@@ -0,0 +1,52 @@
+import { css, html, LitElement } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
+
+@customElement('word-viewer')
+class WordViewer extends LitElement {
+  static styles = css`
+    :host {
+      background-color: white;
+      color: violet;
+      cursor: pointer;
+      display: block;
+    }
+    pre {
+      padding: 0.2em;
+    }
+    /* TODO: Add .backwards styles */
+  `;
+
+  @state() private playDirection = 1;
+  @state() private idx = 0;
+  @property() words: string = 'initial value';
+
+  private intervalTimer?: number;
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.intervalTimer = setInterval(this.tickToNextWord, 1000);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    clearInterval(this.intervalTimer);
+    this.intervalTimer = undefined;
+  }
+
+  render() {
+    const splitWords = this.words.split('.');
+    const word = splitWords[((this.idx % splitWords.length) + splitWords.length) % splitWords.length];
+    return html`<pre
+      class="TODO: Use classMap expression"
+      @click=${this.switchPlayDirection}
+    >${word}</pre>`;
+  }
+
+  tickToNextWord = () => { this.idx += this.playDirection; };
+
+  switchPlayDirection() {
+    this.playDirection *= -1;
+  }
+}
+
