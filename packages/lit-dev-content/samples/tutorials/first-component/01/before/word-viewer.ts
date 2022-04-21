7b77dc3247f9ad79771cffa44da1ad766944a1c7
Andrew Jakubowicz <ajakubowicz@google.com>
2022-04-20 21:41:42 -0700

    Step 1

---
+++ packages/lit-dev-content/samples/tutorials/first-component/01/before/word-viewer.ts
@@ -0,0 +1,15 @@
+import { html, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+// TODO: Register this customElement
+class WordViewer extends LitElement {
+  render() {
+    // TODO: Render something!
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    // TODO: Add "word-viewer" as an HTML tag that TypeScript understands
+  }
+}
