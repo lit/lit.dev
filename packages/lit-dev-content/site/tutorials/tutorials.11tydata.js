6097c8a148639046386d4015866be775a0bc0e0f
e111077 <5981958+e111077@users.noreply.github.com>
2022-04-25 14:32:34 -0700

    add jsdoc for tutorial load data optoins

--- packages/lit-dev-content/site/tutorials/tutorials.11tydata.js
+++ packages/lit-dev-content/site/tutorials/tutorials.11tydata.js
@@ -8,14 +8,32 @@ const fs = require('fs').promises;
 const path = require('path');
 
 /**
+ * 11ty metadata for rendering the featured section in the tutorial catalog.
+ *
+ * @typedef {{position: number}} FeaturedOptions
+ */
+
+/**
+ * Options for Loading Tutorial Metadata passed to 11ty's data rendering system.
+ * @typedef {{
+ *  featured: FeaturedOptions,
+ * }} LoadDataOptions
+*/
+
+/** @type{LoadDataOptions} */
+const defaultOptions = {featured: {position: undefined}};
+
+/**
  * Given a dirname, will load the tutorial's tutorial.json and description.md,
  * files and return a full tutorial data object.
  *
  * @param {string} dirname Name of the tutorial's directory in /samples
+ * @param {Partial<LoadDataOptions>=} Options for loading data.
  * @returns {Promise<Object>} Tutorial data to be consumed by
  *     /tutorials/index.html
  */
-const loadTutorialData = async (dirname, options = {featured: {position: undefined}}) => {
+const loadTutorialData = async (dirname, options = defaultOptions) => {
+  options = {...defaultOptions, ...options};
   const tutorialDir = path.resolve(__dirname, '../../samples/tutorials', dirname);
   const encoding = {encoding: 'utf8'};
 
