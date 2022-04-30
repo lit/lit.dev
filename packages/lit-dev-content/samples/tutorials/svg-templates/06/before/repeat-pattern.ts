b15df71f12e9b67f2696a66af2ad430ce0ad8324
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-04-29 17:46:14 -0700

    add knobs

--- packages/lit-dev-content/samples/tutorials/svg-templates/06/before/repeat-pattern.ts
+++ packages/lit-dev-content/samples/tutorials/svg-templates/06/before/repeat-pattern.ts
@@ -5,35 +5,38 @@ import {customElement, property} from 'lit/decorators.js';
 
 
 const themeCSS = css`
-	.background {
-		fill: var(--background-color, #fff);
-	}
+  .background {
+    fill: var(--background-color, #fff);
+  }
 
-	text {
-		fill: var(--font-color, #000);
-		font-size: var(--font-size, 24px);
-		stroke-width: var(--stroke-width, 1);
-		stroke: var(--stroke-color, #eee);
-	}
+  text {
+    fill: var(--font-color, #000);
+    font-size: var(--font-size, 24px);
+    stroke-width: var(--stroke-width, 1);
+    stroke: var(--stroke-color, #eee);
+  }
 `;
 
 const svgCSS = css`
-	svg {
-		height: 100%;
-		width: 100%;
-	}
+  .knobs {
+    position: absolute;
+  }
+  svg {
+    height: 100%;
+    width: 100%;
+  }
 
-	text {
-		fill: #fff;
-		dominant-baseline: hanging;
-		font-family: monospace;
-		font-size: 24px;
-	}
+  text {
+    fill: #fff;
+    dominant-baseline: hanging;
+    font-family: monospace;
+    font-size: 24px;
+  }
 
-	rect {
-		width: 100%;
-		height: 100%;
-	}
+  rect {
+    width: 100%;
+    height: 100%;
+  }
 `;
 
 const createElement = (chars: string): SVGTemplateResult => svg`
@@ -41,86 +44,106 @@ const createElement = (chars: string): SVGTemplateResult => svg`
 `;
 
 const createMotif = (
-	numPrints: number,
-	offset: number = 0,
+  numPrints: number,
+  offset: number = 0,
 ): SVGTemplateResult => {
-	const rotation = 360 / numPrints;
+  const rotation = 360 / numPrints;
 
-	const prints = [];
-	let currRotation = offset;
-	for (let index = 0; index < numPrints; index++) {
-		currRotation += rotation;
-		prints.push(svg`
-			<use
-				href="#chars"
-				transform="rotate(${currRotation}, 0, 0)">
-			</use>
-    	`)
-	}
+  const prints = [];
+  let currRotation = offset;
+  for (let index = 0; index < numPrints; index++) {
+    currRotation += rotation;
+    prints.push(svg`
+      <use
+        href="#chars"
+        transform="rotate(${currRotation}, 0, 0)">
+      </use>
+      `)
+  }
 
-	return svg
-		`<g
-			id="motif"
-			transform="translate(50, 50)">
-				${prints}
-		</g>`;
+  return svg
+    `<g
+      id="motif"
+      transform="translate(50, 50)">
+        ${prints}
+    </g>`;
 };
 
 const createTileBoundary = () => svg`
-	<clipPath id="rect-clip">
-		<rect width="200" height="200"></rect>
-	</clipPath>
+  <clipPath id="rect-clip">
+    <rect width="200" height="200"></rect>
+  </clipPath>
 `;
 
 const createTile = () => svg`
-	<g clip-path="url(#rect-clip)">
-		<use transform="translate(0, 0)" href="#motif"></use>
-		<use transform="translate(0, 100)" href="#motif"></use>
-		<use transform="translate(100, -50)" href="#motif"></use>
-		<use transform="translate(100, 50)" href="#motif"></use>
-		<use transform="translate(100, 150)" href="#motif"></use>
-	</g>
+  <g clip-path="url(#rect-clip)">
+    <use transform="translate(0, 0)" href="#motif"></use>
+    <use transform="translate(0, 100)" href="#motif"></use>
+    <use transform="translate(100, -50)" href="#motif"></use>
+    <use transform="translate(100, 50)" href="#motif"></use>
+    <use transform="translate(100, 150)" href="#motif"></use>
+  </g>
 `;
 
 const createRepeatPattern = () => svg`
-	<pattern
-		id="repeat-pattern"
-		x="-10"
-		y="-10"
-		width="200"
-		height="200"
-		patternUnits="userSpaceOnUse">
-		${createTile()}
-	</pattern>
+  <pattern
+    id="repeat-pattern"
+    x="-10"
+    y="-10"
+    width="200"
+    height="200"
+    patternUnits="userSpaceOnUse">
+    ${createTile()}
+  </pattern>
 `;
 
 @customElement('repeat-pattern')
 export class RepeatPattern extends LitElement {
-	static styles = [svgCSS, themeCSS];
+  static styles = [svgCSS, themeCSS];
 
-	@property({type: String}) chars = "lit";
-	@property({type: Number, attribute: "num-prints"}) numPrints = 7;
-	@property({
-		type: Number,
-		attribute: "rotation-offset",
-	}) rotationOffset = 0;
+  @property({type: String}) chars = "lit";
+  @property({type: Number, attribute: "num-prints"}) numPrints = 7;
+  @property({
+    type: Number,
+    attribute: "rotation-offset",
+  }) rotationOffset = 0;
 
-	render() {
-		return html`
-			<svg>
-				<defs>
-					${createTileBoundary()}
-					${createElement(this.chars)}
-					${createMotif(
-						this.numPrints,
-						this.rotationOffset,
-					)}
-					${createRepeatPattern()}
-				</defs>
+  render() {
+    return html`
+    <div class="knobs">
+      <input .value=${this.chars} @input=${this.onChars}>
+      <input type="range" min="2" max="29" .value=${this.numPrints} @input=${this.onNumPrints}>
+      <input type="range" min="0" max="360" .value=${this.rotationOffset} @input=${this.onRotationOffset}>
+    </div>
+      <svg>
+        <defs>
+          ${createTileBoundary()}
+          ${createElement(this.chars)}
+          ${createMotif(
+            this.numPrints,
+            this.rotationOffset,
+          )}
+          ${createRepeatPattern()}
+        </defs>
 
-				<rect class="background"></rect>
-				<rect fill="url(#repeat-pattern)"></rect>
-			</svg>
-		`;
-	}
+        <rect class="background"></rect>
+        <rect fill="url(#repeat-pattern)"></rect>
+      </svg>
+    `;
+  }
+
+  onChars = (e: Event) => {
+    if (!(e.target instanceof HTMLInputElement)) return;
+    this.chars = e.target.value
+  }
+
+  onNumPrints = (e: Event) => {
+    if (!(e.target instanceof HTMLInputElement)) return;
+    this.numPrints = e.target.valueAsNumber;
+  }
+
+  onRotationOffset = (e: Event) => {
+    if (!(e.target instanceof HTMLInputElement)) return;
+    this.rotationOffset = e.target.valueAsNumber;
+  }
 }
