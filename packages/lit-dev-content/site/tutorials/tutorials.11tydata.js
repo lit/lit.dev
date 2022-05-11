15cb812b5f4213c10d4267b0846005d3f0ac6c65
Arthur Evans <arthure@google.com>
2022-05-11 10:29:27 -0700

    Make titles consistently sentence case; set featured tutorials. (#824)

--- packages/lit-dev-content/site/tutorials/tutorials.11tydata.js
+++ packages/lit-dev-content/site/tutorials/tutorials.11tydata.js
@@ -66,16 +66,16 @@ module.exports = async () => {
   const tutorials = await Promise.all([
     // Learn
     loadTutorialData('intro-to-lit', {featured: {position: 0}}),
-    loadTutorialData('working-with-lists'),
+    loadTutorialData('working-with-lists', {featured: {position: 1}}),
     loadTutorialData('reactivity'),
-    loadTutorialData('custom-attribute-converter', {featured: {position: 2}}),
-    loadTutorialData('async-directive', {featured: {position: 1}}),
+    loadTutorialData('custom-attribute-converter'),
+    loadTutorialData('async-directive'),
 
 
     // Build
     loadTutorialData('svg-templates'),
     loadTutorialData('tooltip'),
-    loadTutorialData('carousel'),
+    loadTutorialData('carousel', {featured: {position: 2}}),
     loadTutorialData('word-viewer'),
 
     // Draft
