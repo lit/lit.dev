4bf943f97affeaf069d49a574dc1aebdfabe89f0
e111077 <5981958+e111077@users.noreply.github.com>
2022-05-23 15:48:23 -0700

    SSR the searchbox

---
+++ packages/lit-dev-content/src/global/hydrate-litdev-search.ts
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+
+const searches = Array.from(document.body.querySelectorAll('litdev-search'));
+for (const search of searches) {
+  search.addEventListener(
+    'focus',
+    async () => {
+      await import('../components/litdev-search.js');
+
+      search.focus();
+    },
+    {once: true}
+  );
+}
