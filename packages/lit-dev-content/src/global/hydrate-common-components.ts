cb305f7833e77cb67bbfcaf1f6dded3eadf70018
e111077 <5981958+e111077@users.noreply.github.com>
2022-05-23 18:38:40 -0700

    shuffle imports around

--- packages/lit-dev-content/src/global/hydrate-common-components.ts
+++ packages/lit-dev-content/src/global/hydrate-common-components.ts
@@ -3,8 +3,6 @@
  * Copyright 2022 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
  */
-import '../components/lazy-svg.js';
-
 const searches = Array.from(document.body.querySelectorAll('litdev-search'));
 
 for (const search of searches) {
