2a797fcce6c785fdfaa2415909b6d9ef602e3237
e111077 <5981958+e111077@users.noreply.github.com>
2022-06-08 10:24:07 -0700

    prevents needless second request in prod

--- packages/lit-dev-content/src/global/lit-hydrate-support.ts
+++ packages/lit-dev-content/src/global/lit-hydrate-support.ts
@@ -1,1 +1,4 @@
+// This file is needed for rollup bundling order despite it never being loaded
+// in Production. Loading this file directly in prod in default.html would cause
+// an unnecessary second request to lit.js
 import 'lit/experimental-hydrate-support.js';
