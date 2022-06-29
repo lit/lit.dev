58fbeaf8178a5c71dba59459c7e5afacc0cde033
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-06-28 18:17:54 -0700

    remove jsx from tsconfig, missing semicolon

--- packages/lit-dev-content/samples/examples/react-hello-world/index.tsx
+++ packages/lit-dev-content/samples/examples/react-hello-world/index.tsx
@@ -1,6 +1,6 @@
 import { React, ReactDOM } from './deps.js';
 
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 const root = ReactDOM.createRoot(body!);
 
 root.render(<div>{"buster"}</div>);
