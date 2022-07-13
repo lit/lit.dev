a14279b822b36ea4a10d6fc419be9663cf05a1bb
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-07-13 16:07:34 -0700

    better function syntax

--- packages/lit-dev-content/samples/examples/react-events/index.tsx
+++ packages/lit-dev-content/samples/examples/react-events/index.tsx
@@ -7,12 +7,7 @@ const CounterButton = createComponent(React, 'counter-button', CounterButtonWC);
 const App = () => {
   const [count, setCount] = React.useState(0);
 
-  const clickCallback = React.useCallback(
-    (e) => {
-      setCount(count + 1);
-    },
-    [count]
-  );
+  const clickCallback = React.useCallback((e) => setCount(count + 1), [count]);
 
   return <CounterButton onClick={clickCallback} count={count}></CounterButton>;
 };
