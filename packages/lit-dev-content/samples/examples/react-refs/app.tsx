d83d97e81aa200926d89b9054b27087d7af7f830
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-07-27 14:37:09 -0700

    use event state as app state

--- packages/lit-dev-content/samples/examples/react-refs/app.tsx
+++ packages/lit-dev-content/samples/examples/react-refs/app.tsx
@@ -2,7 +2,7 @@ import type {EventName} from '@lit-labs/react';
 import type {StateEvent} from './wander-boids.js';
 
 import {createComponent} from '@lit-labs/react';
-import {React} from "./react.js";
+import {React} from './react.js';
 import {WanderBoids as WanderBoidsWC} from './wander-boids.js';
 
 /*
@@ -10,47 +10,68 @@ import {WanderBoids as WanderBoidsWC} from './wander-boids.js';
   So we want to react to changes in it's state.
   We do that through callbacks and refs
 */
-const WanderBoid = createComponent(React, 'wander-boids', WanderBoidsWC, {onStateEvent: 'state-event' as EventName<StateEvent>});
+const WanderBoid = createComponent(
+  React,
+  'wander-boids',
+  WanderBoidsWC, 
+  {
+    onComponentState: 'state-event' as EventName<CustomEvent<StateEvent>>,
+  }
+);
+
+const defaultAppState: StateEvent = {
+  isPlaying: false,
+  fps: 24,
+};
 
 export const App = () => {
   const boidRef = React.useRef();
 
-  const [state, setState] = React.useState({ isPlaying: false, fps: 24});
+  const [state, setState] = React.useState(defaultAppState);
 
   const play = React.useCallback(() => {
     boidRef.current?.play();
-  }, [boidRef]);
+  }, []);
 
   const pause = React.useCallback(() => {
     boidRef.current?.pause();
-  }, [boidRef]);
+  }, []);
 
   const onChange = React.useCallback((e) => {
     if (boidRef.current === null) {
       return;
     }
     boidRef.current.fps = e.target.value;
-  }, [boidRef]);
+  }, []);
 
-  const onStateEvent = React.useCallback((e: CustomEvent<StateEvent>) => {
+  const onComponentState = React.useCallback((e: CustomEvent<StateEvent>) => {
     if (boidRef.current === null) {
       return;
     }
     setState(e.detail);
-  }, [boidRef, setState]);
-
-  const isPlayEnabled = state.isPlaying ? true : "";
-  const isPauseEnabled = !state.isPlaying ?  true : "";
+  }, []);
 
   return (
     <>
-      <WanderBoid onStateEvent={onStateEvent} ref={boidRef}>
-      </WanderBoid>
+      <WanderBoid
+        onComponentState={onComponentState}
+        ref={boidRef}
+      ></WanderBoid>
       <div>
-        <button disabled={isPlayEnabled} onClick={play}>play</button>
-        <button disabled={isPauseEnabled} onClick={pause}>pause</button>
-        <input onChange={onChange} type="range" value={state.ps} min="6" max="35"></input>
+        <button disabled={state.isPlaying ? true : ''} onClick={play}>
+          play
+        </button>
+        <button disabled={state.isPlaying ? '' : true} onClick={pause}>
+          pause
+        </button>
+        <input
+          onChange={onChange}
+          type="range"
+          value={state.fps}
+          min="6"
+          max="35"
+        ></input>
       </div>
     </>
-  )
-}
+  );
+};
