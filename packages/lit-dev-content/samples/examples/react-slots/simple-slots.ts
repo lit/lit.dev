0990a27bc2abaf1a6c41d61204e6ef6da82bdc90
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-07-28 13:37:16 -0700

    associate slots with color

--- packages/lit-dev-content/samples/examples/react-slots/simple-slots.ts
+++ packages/lit-dev-content/samples/examples/react-slots/simple-slots.ts
@@ -3,21 +3,26 @@ import { customElement, property } from 'lit/decorators.js';
 
 const styles = css`
   :host {
-    display: flex;
     border: 4px solid #343434;
+    box-sizing: border-box;
+    display: flex;
+    gap: 8px;
+    padding: 8px;
   }
 
   slot {
-    display: block;
-    margin: 8px;
-    padding: 8px;
-    border: 4px solid #343434;
     border-radius: 4px;
+    border: 4px solid #343434;
+    display: block;
+    padding: 8px;
   }
 
-  p {
-    margin: 8px;
-    font-family: monospace;
+  div:nth-child(1) {
+    color: blue;
+  }
+
+  div:nth-child(3) {
+    color: purple;
   }
 `;
 
