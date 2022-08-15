41f486fac032f8bffc4d2bad202eb3d597d6978a
novenn <1561755759@qq.com>
2022-08-15 12:20:45 +0800

    support checkout specified lang

--- packages/lit-dev-i18n/bin/checkout/index.js
+++ packages/lit-dev-i18n/bin/checkout/index.js
@@ -1,8 +1,12 @@
-const path   = require("path");
-const fs     = require("fs");
+const path     = require("path");
+const fs       = require("fs");
+const minimist = require("minimist")
 const { merge, create }    = require("./lib");
+const args = minimist(process.argv);
+const lang = args.lang
 const remote = path.resolve(__dirname, "../../src/index.md");
-const workspace = path.resolve(__dirname, "../../workspace/index.xml");
+const workspace = path.resolve(__dirname, "../../workspace/", lang, "./index.xml");
+console.log(workspace);
 
 function checkout(remote, workspace) {
   if(fs.existsSync(workspace)) {
--- packages/lit-dev-i18n/src/index.md
+++ packages/lit-dev-i18n/src/index.md
@@ -7,7 +7,5 @@ this is overview of lit.dev
 # Start using lit.dev
 
 - first item
-- second item 1234333 dd sedd
-- third item ewdd 
-- hahaha 22
-- hahaha
+- second item
+- third item 
