39aa930806db29fd1f86a671f26fc472646e885d
Elliott Marquez <5981958+e111077@users.noreply.github.com>
2022-09-02 15:20:08 -0700

    refactor out into functions and clean up code

--- packages/lit-dev-discord-bot/src/index.ts
+++ packages/lit-dev-discord-bot/src/index.ts
@@ -5,6 +5,7 @@
  */
 
 import {
+  AutocompleteInteraction,
   ChatInputCommandInteraction,
   Client,
   GatewayIntentBits,
@@ -38,99 +39,143 @@ interface Suggestion {
   title: string;
 }
 
+type PublishableCommand = Omit<
+  SlashCommandBuilder,
+  'addSubcommand' | 'addSubcommandGroup'
+>;
+
 // Build the UI for the slash command.
-const command = new SlashCommandBuilder()
+const docsSlashCommand = new SlashCommandBuilder()
   .setName('docs')
-  .setDescription('Will search lit.dev.')
+  .setDescription('Search lit.dev docs.')
   .addStringOption((option) =>
     option
       .setName('query')
-      .setDescription('The query to search for.')
+      .setDescription('The text to search for.')
       .setRequired(true)
       .setAutocomplete(true)
   );
 
-const rest = new REST({version: '10'}).setToken(BOT_CLIENT_SECRET);
-
-(async () => {
+/**
+ * Publishes the slash commands to the Discord server.
+ *
+ * @param commands Commands to publish to the Discord server.
+ */
+const publishDiscordCommands = async (commands: PublishableCommand[]) => {
+  const rest = new REST({version: '10'}).setToken(BOT_CLIENT_SECRET);
   try {
     console.log('Started refreshing application (/) commands.');
 
     // Tell Discord that we publish the following slash commands.
     await rest.put(Routes.applicationCommands(publicVars.discord.clientId), {
-      body: [command],
+      body: commands,
     });
 
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
     console.error(error);
   }
-})();
+};
 
-const client = new Client({intents: [GatewayIntentBits.Guilds]});
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user?.tag}!`);
-});
-
-client.on('interactionCreate', async (interaction) => {
-  if ((interaction as ChatInputCommandInteraction).commandName !== 'docs') {
+/**
+ * Handles the autocomplete interaction for the /docs slash command. Searches
+ * the lit.dev algolia index and returns the formatted results to the user where
+ * the value is the lit.dev url.
+ *
+ * @param interaction The Autocomplete interaction to be handled.
+ */
+const handleDocsAutocompleteInteraction = async (
+  interaction: AutocompleteInteraction
+) => {
+  const focusedValue = interaction.options.getFocused();
+  // Do not waste a query if the user has fewer than 3 chars.
+  if (focusedValue.length < 3) {
+    await interaction.respond([]);
     return;
   }
 
-  // This happens as the user is typing. Enabled by the SlashCommandBuilder's
-  // .setAutocomplete(true) option.
-  if (interaction.type === InteractionType.ApplicationCommandAutocomplete) {
-    const focusedValue = interaction.options.getFocused();
-    // Do not waste a query if the user has fewer than 3 chars.
-    if (focusedValue.length < 3) {
-      await interaction.respond([]);
+  // Search algolia for the query.
+  const searchRes = await index.search<Suggestion>(focusedValue, {
+    page: 0,
+    hitsPerPage: 5,
+  });
+
+  // Transform the hits' relative URL to objects that are readable and
+  // linkable outside of lit.dev.
+  const results = searchRes.hits.map((hit) => {
+    const readableText = hit.isSubsection
+      ? `${hit.title} - ${hit.heading}`
+      : hit.title;
+
+    // autocomplete requires a `name` and a `value` property like a <select>
+    // The name is what is shown to the user, but the `value` is what is
+    // actually sent to the bot in the `.isChatInputCommand()` event.
+    return {
+      name: readableText,
+      value: `https://lit.dev${hit.relativeUrl}`,
+    };
+  });
+
+  await interaction.respond(results);
+};
+
+/**
+ * Checks that the value of the /docs interaction is a lit.dev url and then
+ * sends a message to the channel with the link or will display an ephemeral
+ * error message visible only to the invoker.
+ *
+ * @param interaction The /docs command interaction to be handled.
+ */
+const handleDocsSubmissionInteraction = async (
+  interaction: ChatInputCommandInteraction
+) => {
+  const value = interaction.options.data[0].value as string;
+
+  // If the response is a lit.dev URL then tell the bot to post it.
+  if (value.startsWith('https://lit.dev')) {
+    await interaction.reply({content: value});
+  } else {
+    // If the response is not a lit.dev url, then bot responds with an
+    // ephemeral message that is only visible to the user. This happens when
+    // there are no results, or if the user hits enter before results show up.
+    interaction.reply({
+      ephemeral: true,
+      content: `value: "${value}" is not a valid lit.dev url. Please select from the autocomplete list.`,
+    });
+  }
+};
+
+/**
+ * Creates a discord bot client, registers the event handlers, and starts the
+ * client websocket server that listens for events.
+ */
+const startClientWebsocketServer = async () => {
+  const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+  client.on('ready', () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+  });
+
+  client.on('interactionCreate', async (interaction) => {
+    if ((interaction as ChatInputCommandInteraction).commandName !== 'docs') {
       return;
     }
 
-    // Search algolia for the query.
-    const searchRes = await index.search<Suggestion>(focusedValue, {
-      page: 0,
-      hitsPerPage: 5,
-    });
-
-    // Transform the hits' relative URL to objects that are readable and
-    // linkable outside of lit.dev.
-    const results = searchRes.hits.map((hit) => {
-      const readableText = hit.isSubsection
-        ? `${hit.title} - ${hit.heading}`
-        : hit.title;
-
-      // autocomplete requires a `name` and a `value` property like a <select>
-      // The name is what is shown to the user, but the `value` is what is
-      // actually sent to the bot in the `.isChatInputCommand()` event.
-      return {
-        name: readableText,
-        value: `https://lit.dev${hit.relativeUrl}`,
-      };
-    });
-
-    await interaction.respond(results);
-  }
-
-  // This is true when the user finally returns a command. (a lit.dev url)
-  if (interaction.isChatInputCommand()) {
-    const value = interaction.options.data[0].value as string;
-
-    // If the response is a lit.dev URL then tell the bot to post it.
-    if (value.startsWith('https://lit.dev')) {
-      await interaction.reply({content: value});
-    } else {
-      // If the response is not a lit.dev url, then bot responds with an
-      // ephemeral message that is only visible to the user. This happens when
-      // there are no results, or if the user hits enter before results show up.
-      interaction.reply({
-        ephemeral: true,
-        content: `value: "${value}" is not a valid lit.dev url. Please select from the autocomplete list.`,
-      });
+    // This happens as the user is typing. Enabled by the SlashCommandBuilder's
+    // .setAutocomplete(true) option.
+    if (interaction.type === InteractionType.ApplicationCommandAutocomplete) {
+      handleDocsAutocompleteInteraction(interaction);
     }
-  }
-});
 
-// Start the web socket connection to the Bot.
-client.login(BOT_CLIENT_SECRET);
+    // This is true when the user finally returns a command. (a lit.dev url)
+    if (interaction.isChatInputCommand()) {
+      handleDocsSubmissionInteraction(interaction);
+    }
+  });
+
+  // Start the web socket connection to the Bot.
+  client.login(BOT_CLIENT_SECRET);
+};
+
+publishDiscordCommands([docsSlashCommand]);
+startClientWebsocketServer();
