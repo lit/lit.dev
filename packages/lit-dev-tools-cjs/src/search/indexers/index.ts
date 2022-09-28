3f8c54694c9aa62d5a1cd4d6f487adbec921a3c3
Elliott Marquez <5981958+e111077@users.noreply.github.com>
2022-09-27 17:52:13 -0700

    Refactor index generator

---
+++ packages/lit-dev-tools-cjs/src/search/indexers/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+export {UrlToFile} from './utils';
+export {indexDocs} from './index-docs';
+export {indexArticles} from './index-articles';
+export {indexApi} from './index-api';
