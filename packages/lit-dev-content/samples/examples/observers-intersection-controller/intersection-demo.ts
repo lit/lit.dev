a6a87b149ae957393e716b29d831163bcdd19024
Andrew Jakubowicz <ajakubowicz@google.com>
2022-10-13 13:25:46 -0700

    code review feedback from sorvell - thank you!

--- packages/lit-dev-content/samples/examples/observers-intersection-controller/intersection-demo.ts
+++ packages/lit-dev-content/samples/examples/observers-intersection-controller/intersection-demo.ts
@@ -8,14 +8,37 @@ import {SHAPES} from './constants.js';
 import {styles} from './styles.js';
 
 /**
- * Show how a @lit-labs/observers IntersectionController can manage multiple
- * child elements. This component generates multiple elements, and then toggles
- * a class as they come in and out of the viewport.
+ * Show how a @lit-labs/observers IntersectionController can observe multiple
+ * child elements. This component generates multiple shape elements, and then
+ * toggles a class as they come in and out of the viewport.
  */
 @customElement('intersection-demo')
-export class MyElement extends LitElement {
-  private intersectionController!: IntersectionController;
+export class IntersectionDemo extends LitElement {
   static styles = styles;
+
+  intersectionController = new IntersectionController(this, {
+    // Prevent the controller from observing the host element.
+    target: null,
+    config: {
+      threshold: 0.2,
+      // Required so the Intersection observer uses playground iframe for
+      // viewport checking.
+      root: document.body,
+      // Modify the viewport intersection check, so the top and bottom 100px
+      // of the preview is "outside the viewport". This helps visualize the
+      // transition of the elements in and out of view.
+      rootMargin: `-100px 0px -100px 0px`,
+    },
+    callback(entries) {
+      for (const {isIntersecting, target} of entries) {
+        if (isIntersecting) {
+          target.classList.remove('hidden');
+        } else {
+          target.classList.add('hidden');
+        }
+      }
+    },
+  });
 
   render() {
     return html`
@@ -35,29 +58,7 @@ export class MyElement extends LitElement {
   }
 
   firstUpdated() {
-    this.intersectionController = new IntersectionController(this, {
-      // Prevent the controller from observing the host element.
-      target: null,
-      config: {
-        threshold: 0.2,
-        // Required so the Intersection observer uses playground iframe for
-        // viewport checking.
-        root: document.body,
-        // Modify the viewport intersection check, so the top and bottom 100px
-        // of the preview is "outside the viewport". This helps visualize the
-        // transition of the elements in and out of view.
-        rootMargin: `-100px 0px -100px 0px`,
-      },
-      callback(entries) {
-        for (const {isIntersecting, target} of entries) {
-          if (isIntersecting) {
-            target.classList.remove('hidden');
-          } else {
-            target.classList.add('hidden');
-          }
-        }
-      },
-    });
+    // Observe all the shapes!
     this.shadowRoot
       ?.querySelectorAll('#parallax div')
       .forEach((item) => this.intersectionController.observe(item));
@@ -66,6 +67,6 @@ export class MyElement extends LitElement {
 
 declare global {
   interface HTMLElementTagNameMap {
-    'intersection-demo': MyElement;
+    'intersection-demo': IntersectionDemo;
   }
 }
