a4f2d45b82c6465dfcc04480df72890b029dd86f
Elliott Marquez <5981958+e111077@users.noreply.github.com>
2022-10-25 15:08:34 -0700

    kick screenshot tests again

--- packages/lit-dev-content/src/components/litdev-cookie-banner.ts
+++ packages/lit-dev-content/src/components/litdev-cookie-banner.ts
@@ -3,7 +3,6 @@
  * Copyright 2022 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
  */
-
 import {css, html, LitElement} from 'lit';
 import {customElement} from 'lit/decorators.js';
 import './litdev-ripple-icon-button.js';
