42b1ddbccab0b5f72f193f35b114ba716535aa35
Elliott Marquez <5981958+e111077@users.noreply.github.com>
2023-04-04 15:18:01 -0700

    bump lit-ssr version

--- packages/lit-dev-content/src/global/dsd-polyfill.ts
+++ packages/lit-dev-content/src/global/dsd-polyfill.ts
@@ -1,6 +1,6 @@
 import {hydrateShadowRoots} from '@webcomponents/template-shadowroot/template-shadowroot.js';
 
-if (!HTMLTemplateElement.prototype.hasOwnProperty('shadowRoot')) {
+if (!HTMLTemplateElement.prototype.hasOwnProperty('shadowRootMode')) {
   hydrateShadowRoots(document.body);
 }
 document.body.removeAttribute('dsd-pending');
