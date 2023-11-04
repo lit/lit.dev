48845d0f30d90cd9b1352912d9995de14af3ff4a
vandeurenglenn <vandeurenglenn@gmail.com>
2023-11-04 15:33:51 +0100

    Eleventy doesn't like lit-dev-content as a module

--- packages/lit-dev-content/scripts/fonts.js
+++ packages/lit-dev-content/scripts/fonts.js
@@ -1,27 +1,27 @@
-import rimraf from 'rimraf'
-import {mkdir, copyFile} from 'fs/promises'
-import {execSync} from 'child_process'
-import glob from 'globby'
-import {parse} from 'path'
+const rimraf = require('rimraf')
+const {mkdirSync, copyFileSync} = require('fs')
+const {execFileSync, execSync} = require('child_process')
+const glob = require('globby')
+const {parse} = require('path')
+
+
 
 rimraf.sync('temp')
 
 try {
-  await mkdir('site/fonts/manrope')
+  mkdirSync('site/fonts/manrope')
 } catch {
   // already exists
 }
 
 // clone the fonts repo
 // TODO: check if the folder already exists
-execSync('git clone https://github.com/sharanda/manrope.git temp/manrope')
+execFileSync('git', ['clone', 'https://github.com/sharanda/manrope.git', 'temp/manrope'])
 execSync('cd temp/manrope && git checkout 9ffbc349f4659065b62f780fe6e9d5a93518bd95')
 // get the desired font files to copy
-const files = await glob('temp/manrope/fonts/web/*.woff2')
+const files = glob.sync('temp/manrope/fonts/web/*.woff2')
 // copy the files to site/fonts/manrope
-const promises = []
 for (const file of files) {
   const parsed = parse(file)
-  promises.push(copyFile(file, `site/fonts/manrope/${parsed.name}${parsed.ext}`))
+  copyFileSync(file, `site/fonts/manrope/${parsed.name}${parsed.ext}`)
 }
-await Promise.all(promises)
