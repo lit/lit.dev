5023d0e8b928848ed0d8875527323a4af07b3cfa
Elliott Marquez <5981958+e111077@users.noreply.github.com>
2024-02-28 17:29:29 -0800

    format

--- packages/lit-dev-tests/src/playwright/learn.spec.ts
+++ packages/lit-dev-tests/src/playwright/learn.spec.ts
@@ -14,7 +14,7 @@ function runScreenshotTests(dark: boolean) {
         viewport: {width: 1920, height: 1080},
       });
       // stop animations for fewer flakes
-      page.emulateMedia({'reducedMotion': 'reduce'});
+      page.emulateMedia({reducedMotion: 'reduce'});
 
       await preventGDPRBanner(page);
       await page.goto('/learn/');
